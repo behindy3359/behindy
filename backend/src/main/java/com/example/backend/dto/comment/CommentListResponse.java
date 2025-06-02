066f9c6d6bd8912657b070105c99b05db1d1b6f0
behindy3359 <solme47@gmail.com>
2025-06-02 23:35:07 +0900

    feat : 댓글 수정/삭제 - 1

--- backend/src/main/java/com/example/backend/dto/comment/CommentListResponse.java
+++ backend/src/main/java/com/example/backend/dto/comment/CommentListResponse.java
@@ -1,4 +1,20 @@
 package com.example.backend.dto.comment;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentListResponse {
+    private List<CommentListResponse> coments;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
 }
