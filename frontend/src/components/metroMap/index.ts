fbbfde97b9a798d5923d8ce8bd93bd6d04f0f958
behindy3359 <solme47@gmail.com>
2025-07-05 23:42:45 +0900

    nx-fix : 메인 레이아웃 수정 - 6

--- frontend/src/components/metroMap/index.ts
+++ frontend/src/components/metroMap/index.ts
@@ -1,6 +1,5 @@
 // 나중에 추가될 컴포넌트들
 
-import { MetroLineAnimations } from './MetroLineAnimations';
 export { RealtimeMetroMap } from './RealtimeMetroMap';
 
 // export { InteractiveMetroMap } from './InteractiveMetroMap';
