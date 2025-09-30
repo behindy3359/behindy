554ad1bf0b23b822f6f3fbfa1a54c24e691170ef
behindy3359 <solme47@gmail.com>
2025-09-30 23:44:36 +0900

    nx-fix : 프론트엔드 리팩토링 - 24

--- backend/src/main/java/com/example/backend/service/MetroCacheService.java
+++ backend/src/main/java/com/example/backend/service/MetroCacheService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
